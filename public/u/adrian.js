2d8edffadeb360c9cc7b84d4479318299a8ca4fb
Will <11353590+june07@users.noreply.github.com>
2024-07-25 12:29:08 -0700

    .

--- public/u/adrian.js
+++ public/u/adrian.js
@@ -1,15 +1,36 @@
 
 const links = [
     {
-        "url": "https://b.com",
-        "title": "b title",
-        "subtitle": "b subtitle"
+        url: "https://june07.com",
+        title: "June07",
+        subtitle: "Blog"
     },
     {
-        "url": "https://c.com"
+        url: "https://nim.june07.com",
+        title: "Node --inspector Manager",
+        subtitle: 'Streamline your Node.js Debugging Workflow'
     },
     {
-        "url": "https://a.com"
+        url: "https://github.com/june07",
+        title: "GitHub Account"
+    },
+    {
+        url: 'https://www.npmjs.com/~667'
+    },
+    {
+        url: 'https://ngrok-dns.june07.com/'
+    },
+    {
+        url: 'https://policies.june07.com/'
+    },
+    {
+        url: 'https://packagecontrol.io/packages/Jenkinsfile'
+    },
+    {
+        url: 'https://github.com/synchook'
+    },
+    {
+        url: 'https://brakecode.com'
     }
 ];
 const rules = [];
