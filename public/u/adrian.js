4f7164f9368730ed73f359ade33db3c2e0f408ef
Will <11353590+june07@users.noreply.github.com>
2024-07-25 13:30:40 -0700

    .

--- public/u/adrian.js
+++ public/u/adrian.js
@@ -3,12 +3,14 @@ const links = [
     {
         url: "https://june07.com",
         title: "June07",
-        subtitle: "Blog"
+        subtitle: "Blog",
+        favicon: 'https://github.june07.com/image/favicons/june07.com/favicon.ico'
     },
     {
         url: "https://nim.june07.com",
         title: "Node --inspector Manager",
-        subtitle: 'Streamline your Node.js Debugging Workflow'
+        subtitle: 'Streamline your Node.js Debugging Workflow',
+        favicon: 'https://nim.june07.com/icon128@3x.png'
     },
     {
         url: "https://github.com/june07",
@@ -18,19 +20,23 @@ const links = [
         url: 'https://www.npmjs.com/~667'
     },
     {
-        url: 'https://ngrok-dns.june07.com/'
+        url: 'https://ngrok-dns.june07.com/',
+        favicon: 'https://ngrok-dns.june07.com/icon-512x512-t.png'
     },
     {
-        url: 'https://policies.june07.com/'
+        url: 'https://policies.june07.com/',
+        favicon: 'https://policies.june07.com/favicon.ico?v=QEwWO2984R'
     },
     {
-        url: 'https://packagecontrol.io/packages/Jenkinsfile'
+        url: 'https://packagecontrol.io/packages/Jenkinsfile',
     },
     {
-        url: 'https://github.com/synchook'
+        url: 'https://github.com/synchook',
+        favicon: 'https://avatars.githubusercontent.com/u/123592097?s=48&v=4'
     },
     {
-        url: 'https://brakecode.com'
+        url: 'https://brakecode.com',
+        favicon: 'https://brakecode.com/favicon-32x32.png?v=yy42aWEbv9'
     }
 ];
 const rules = [];
